c56e49c8c2c43c2d2b310e1be0f5bf944dab1c98
𝔬𝔵𝔩 <egodominus0x1@gmail.com>
2025-07-17 03:23:07 +0330

    Add chessboard pattern generation example
    Commented out the FizzBuzz example and added a new code block that generates and prints an 8x8 chessboard pattern to the console.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -3,15 +3,29 @@
 //     console.log("#".repeat(i));
 // }
 
-// FizzBuzz
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
+// // FizzBuzz
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// Chessboard
+for (let i = 0; i < 8; i++) {
+    let row = "";
+    for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+            row += "#";
+        } else {
+            row += " ";
+        }
     }
+    console.log(row);
 }
+
