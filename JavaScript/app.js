bad44bfe27472535c9137a175567556df5ddaa11
𝔬𝔵𝔩 <egodominus0x1@gmail.com>
2025-07-17 03:21:42 +0330

    Add FizzBuzz implementation in app.js
    Implemented the classic FizzBuzz problem, printing numbers from 1 to 100 with 'Fizz', 'Buzz', or 'FizzBuzz' for multiples of 3, 5, or both. Also included a commented-out solution for looping a triangle.

---
+++ JavaScript/app.js
@@ -0,0 +1,17 @@
+// // Looping a Triangle
+// for (let i = 1; i <= 7; i++) {
+//     console.log("#".repeat(i));
+// }
+
+// FizzBuzz
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
